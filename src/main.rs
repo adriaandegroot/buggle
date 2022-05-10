bba0601acb6f6ad85d1d85da6850b6ede7793bf6
Adriaan de Groot <groot@kde.org>
2022-05-10 15:26:00 +0200

    SPDX: tag source files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+// Buggle
+//
+// A bug-count social-media program.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-FileCopyrightText: 2022 Adriaan de Groot <groot@kde.org>
+//
+
 fn main() {
     println!("Hello, world!");
 }
